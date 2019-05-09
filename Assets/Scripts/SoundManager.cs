9de31e4c061cdedae47c7ccfbeda3a314f359709
Rodrigo Nietiedt <rodrigo.almeida@aquiris.com.br>
2019-05-09 18:50:02 -0300

    sound

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    [SerializeField]
+    private AudioSource _shot;
+    
+    [SerializeField]
+    private AudioSource _enemySpawn;
+    
+    [SerializeField]
+    private AudioSource _enemyDie;
+    
+    [SerializeField]
+    private AudioSource _uiAccept;
+    
+    [SerializeField]
+    private AudioSource _uiBack;
+
+    private void Awake()
+    {
+        GameEvents.OnShotFired += OnShotFired;
+        GameEvents.OnBirdKilled += OnBirdKilled;
+        GameEvents.OnBirdSpawned += OnBirdSpawned;
+        GameEvents.OnUiAccept += OnUiAccept;
+    }
+
+    private void OnDestroy()
+    {
+        GameEvents.OnShotFired -= OnShotFired;
+        GameEvents.OnBirdKilled -= OnBirdKilled;
+        GameEvents.OnBirdSpawned -= OnBirdSpawned;
+        GameEvents.OnUiAccept -= OnUiAccept;
+    }
+
+    private void OnShotFired()
+    {
+        PlaySfx(_shot);
+        Debug.Log("play shot sound");
+    }
+
+    private void OnBirdKilled()
+    {
+        PlaySfx(_enemyDie);
+    }
+
+    private void OnBirdSpawned()
+    {
+        PlaySfx(_enemySpawn);
+    }
+
+    private void OnUiAccept()
+    {
+        PlaySfx(_uiAccept);
+    }
+    
+    private void PlaySfx(AudioSource sfx)
+    {
+        sfx?.Play();
+    }
+}
