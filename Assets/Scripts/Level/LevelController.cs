038a5e989f0dd11865029261dfe69c88be7057e5
Rodrigo Nietiedt <rodrigonietiedt@gmail.com>
2019-05-30 17:10:02 -0300

    wip

---
+++ Assets/Scripts/Level/LevelController.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelController : MonoBehaviour
+{
+    public int CurrentScore { get; private set; }
+    public int CurrentLevel { get; private set; }
+}
